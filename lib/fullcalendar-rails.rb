80822525ea5f1d1545d1ce0d507e8c721a93d5e1
Jon Riddle <jon@jdrnetworking.com>
2013-06-01 13:53:53 -0500

    Add asset precompile hook

--- lib/fullcalendar-rails.rb
+++ lib/fullcalendar-rails.rb
@@ -3,6 +3,11 @@ require "fullcalendar-rails/version"
 module Fullcalendar
   module Rails
     class Engine < ::Rails::Engine
+      if Rails.version >= '3.1'
+        initializer "fullcalendar-rails.precompile-hook" do |app|
+          app.config.assets.precompile += %w(fullcalendar.js fullcalendar.min.js gcal.js fullcalendar.css fullcalendar.print.css)
+        end
+      end
     end
   end
 end
