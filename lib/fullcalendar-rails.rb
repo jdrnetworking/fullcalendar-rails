755acacc1410359ac0b3fd91bf9b705b44866daa
Jon Riddle <jon@jdrnetworking.com>
2013-06-01 14:14:31 -0500

    Correctly scope Rails for version check

--- lib/fullcalendar-rails.rb
+++ lib/fullcalendar-rails.rb
@@ -3,7 +3,7 @@ require "fullcalendar-rails/version"
 module Fullcalendar
   module Rails
     class Engine < ::Rails::Engine
-      if Rails.version >= '3.1'
+      if ::Rails.version >= '3.1'
         initializer "fullcalendar-rails.precompile-hook" do |app|
           app.config.assets.precompile += %w(fullcalendar.js fullcalendar.min.js gcal.js fullcalendar.css fullcalendar.print.css)
         end
